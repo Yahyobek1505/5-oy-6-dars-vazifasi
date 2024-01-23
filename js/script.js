b3669dbf0342e7b4b32ef832ddc2eeb8062598b4
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-23 21:19:54 +0500

    Save  to LocalStorage is done!

--- js/script.js
+++ js/script.js
@@ -13,8 +13,31 @@ const form = document.querySelector("#form");
 
 button  && button.addEventListener('click', function (event) {
   event.preventDefault();
-  if (validation({Inputname, inputNarx, inputCategory,img, sana,color, tasnif})) {
-    
+  if (validation({Inputname, inputNarx, inputCategory,img, sana, color, tasnif})) {
+    let car = {
+       name: Inputname.value,
+       narx: inputNarx.value,
+       year: sana.value,
+       img: img.value,
+       color: color.value,
+       tasnif: tasnif.value,
+       category: inputCategory.value,
+       status: 'active',
+       id: Date.now()
+    }
+    let data = getData();x
+    data.push(car);
+    localStorage.setItem('cars', JSON.stringify(data));
+    form.reset();
+
+
+    function getData() {
+      let data = [];
+      if (localStorage.getItem('cars')) {
+        data = JSON.parse(localStorage.getItem('cars'));
+      }
+      return data;
+    }
   } else {
     console.log("validatsiyadan o'tmadi.");
   }
